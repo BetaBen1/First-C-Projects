4261b1b61f0f593b89a4e7d18c813977986a1a68
BenHardin <bbhardin1@gmail.com>
2017-06-22 21:23:26 -0400

    In the beginning...

---
+++ Strings/src/Strings.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Strings.cpp
+// Author      : Ben Hardin
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int value = 6;
+
+	cout << value << endl;
+
+	return 0;
+}
