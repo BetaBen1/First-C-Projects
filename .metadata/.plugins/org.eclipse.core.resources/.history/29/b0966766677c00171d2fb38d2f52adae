8b388c4b6dcd4a664360799e03b26d8e596f613f
BenHardin <bbhardin1@gmail.com>
2017-08-08 21:20:52 -0400

    Learnin' Again

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/29/b0966766677c00171d2fb38d2f52adae
@@ -0,0 +1,34 @@
+/*
+ * Do-While Loops!
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	const string password = "hello";
+
+	cout << "Enter your password: " << flush;
+
+	string input;
+	cin >> input;
+
+	/*if (input == password){
+		cout << "Password accepted." << endl;
+	} else {
+		cout << "Access denied." << endl;
+	}*/
+
+	while(input != password){
+		cout << "Access denied." << endl;
+
+		cout << "Enter your password: " << flush;
+		cin >> input;
+	}
+
+	cout << "Password accepted.";
+
+	return 0;
+
+}
--- Do-While/src/Do-While.cpp
+++ Do-While/src/Do-While.cpp
@@ -9,23 +9,16 @@ int main(){
 
 	const string password = "hello";
 
-	cout << "Enter your password: " << flush;
-
 	string input;
-	cin >> input;
 
-	/*if (input == password){
-		cout << "Password accepted." << endl;
-	} else {
-		cout << "Access denied." << endl;
-	}*/
-
-	while(input != password){
-		cout << "Access denied." << endl;
+	do{
 
 		cout << "Enter your password: " << flush;
 		cin >> input;
-	}
+		if(input!=password){
+			cout << "Access denied." << endl;
+		}
+	}while(input!=password);
 
 	cout << "Password accepted.";
 
