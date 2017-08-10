7d34585a779ce2664995e8e1286f6ca0be4114b6
BenHardin <bbhardin1@gmail.com>
2017-08-10 15:12:26 -0400

    I is Learnin' more

--- Break and Continue/src/Break and Continue.cpp
+++ Break and Continue/src/Break and Continue.cpp
@@ -19,17 +19,35 @@ int main(){
 
 	}*/
 
-	for(int i = 0; i < 5; i++){
+	/*for(int i = 0; i < 5; i++){
 
 			cout << "i is: " << i << endl;
 
 			if(i == 3){
-				break;
+				continue;
 			}
 
 			cout << "Looping..." << endl;
 
+		}*/
+
+	const string password = "hello";
+
+	string input;
+
+	do{
+
+		cout << "Enter your password: " << flush;
+		cin >> input;
+
+		if(input==password){
+			break;
+		} else {
+			cout << "Access denied." << endl;
 		}
+	}while(true);
+
+	cout << "Password accepted." << endl;
 
 	cout << "Program quitting..." << endl;
 
