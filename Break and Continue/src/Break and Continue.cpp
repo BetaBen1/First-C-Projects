e7ef149772ec57145d1ba1e9fb12d3accfce91e0
BenHardin <bbhardin1@gmail.com>
2017-08-09 20:36:30 -0400

    A Little Learnin'

--- Break and Continue/src/Break and Continue.cpp
+++ Break and Continue/src/Break and Continue.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(){
 
-	for(int i = 0; i < 5; i++){
+	/*for(int i = 0; i < 5; i++){
 
 		cout << "i is: " << i << endl;
 
@@ -17,7 +17,19 @@ int main(){
 
 		cout << "Looping..." << endl;
 
-	}
+	}*/
+
+	for(int i = 0; i < 5; i++){
+
+			cout << "i is: " << i << endl;
+
+			if(i == 3){
+				break;
+			}
+
+			cout << "Looping..." << endl;
+
+		}
 
 	cout << "Program quitting..." << endl;
 
