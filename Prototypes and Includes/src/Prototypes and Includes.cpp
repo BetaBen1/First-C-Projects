ff682a273b9e595478e5551ed582c5a9280f1258
BenHardin <bbhardin1@gmail.com>
2017-08-29 15:13:06 -0400

    Wow! I learned a lot!
    Headers, Prototypes, and Classes

---
+++ Prototypes and Includes/src/Prototypes and Includes.cpp
@@ -0,0 +1,18 @@
+/*
+ * Prototypes and Includes
+ */
+
+#include <iostream>
+#include "utils.h"
+
+int main(){
+
+	doSomething();
+	doSomething();
+
+	return 0;
+}
+
+void doSomething(){
+	std::cout << "Hello" << std::endl;
+}
---
+++ Prototypes and Includes/src/utils.h
@@ -0,0 +1,13 @@
+/*
+ * utils.h
+ *
+ *  Created on: Aug 29, 2017
+ *      Author: Ben H.
+ */
+
+#ifndef SRC_UTILS_H_
+#define SRC_UTILS_H_
+
+void doSomething();
+
+#endif /* SRC_UTILS_H_ */
